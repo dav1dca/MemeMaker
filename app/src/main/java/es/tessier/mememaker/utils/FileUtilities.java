5f40005f65c0b24a004a9cc68c46e78a7fedbf8f
carlosfernandez <prof.carlos.tessier@gmail.com>
2014-12-28 13:41:45 +0100

    implemented saveAssetImage method

--- app/src/main/java/es/tessier/mememaker/utils/FileUtilities.java
+++ app/src/main/java/es/tessier/mememaker/utils/FileUtilities.java
@@ -1,23 +1,55 @@
 package es.tessier.mememaker.utils;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
+import android.util.Log;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Evan Anger on 7/28/14.
  */
 public class FileUtilities {
 
+    public final static String TAG = FileUtilities.class.getName();
+
     public static void saveAssetImage(Context context, String assetName) {
 
+        File fileDirectory = context.getFilesDir();
+        File fileToWrite = new File(fileDirectory, assetName);
 
+
+        AssetManager assetManager = context.getAssets();
+        InputStream in = null;
+        FileOutputStream out = null;
+        try {
+            in = assetManager.open(assetName);
+            //out = context.openFileOutput(fileToWrite.getAbsolutePath(),context.MODE_PRIVATE);
+
+            out = new FileOutputStream(fileToWrite);
+        } catch (IOException e) {
+            Log.e(TAG, "IOException caught ", e);
+        } finally {
+            if (in != null)
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    Log.e(TAG, "IOException caught ", e);
+                }
+            if (out != null)
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    Log.e(TAG, "IOException caught ", e);
+                }
+        }
 
     }
 
