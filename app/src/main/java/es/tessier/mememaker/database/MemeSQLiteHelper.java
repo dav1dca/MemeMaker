5fb3fdcb3993442aaa0a8428cb9d118c26f3bff7
carlosfernandez <prof.carlos.tessier@gmail.com>
2015-01-05 13:33:31 +0100

    create database

--- app/src/main/java/es/tessier/mememaker/database/MemeSQLiteHelper.java
+++ app/src/main/java/es/tessier/mememaker/database/MemeSQLiteHelper.java
@@ -7,6 +7,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+
 public class MemeSQLiteHelper extends SQLiteOpenHelper {
 
     final static String DB_NAME ="memes.db";
